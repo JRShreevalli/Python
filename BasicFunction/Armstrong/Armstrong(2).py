bb5c8958cfa846f8e037729b43403853e42d89b6
JRShreevalli <70849567+JRShreevalli@users.noreply.github.com>
2021-11-29 18:26:24 +0530

    Add files via upload

---
+++ BasicFunction/Armstrong/Armstrong(2).py
@@ -0,0 +1,30 @@
+
+def order(x):
+  
+    count = 0
+    while (x != 0):
+        count = count + 1
+        x = x // 10
+          
+    return count
+  
+
+def isArmstrong(x):
+      
+    n = order(x)
+    temp = x
+    sum1 = 0
+      
+    while (temp != 0):
+        r = temp % 10
+        sum1 = sum1 + pow(r, n)
+        temp = temp // 10
+
+    return (sum1 == x)
+  
+
+x = int(input())
+print(isArmstrong(x))
+  
+
+
