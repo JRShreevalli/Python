c8bab296148951ec8e4641f3de36253ff1391004
JRShreevalli <70849567+JRShreevalli@users.noreply.github.com>
2021-11-29 16:17:10 +0530

    Add files via upload

---
+++ BasicLevel/Swap/Approach2.py
@@ -0,0 +1,7 @@
+# SECOND APPROACH
+
+
+x = int(input('Enter x value : '))
+y = int(input('Enter y value : '))
+x , y = y , x 
+print(' x = {0}  and y = {1} '.format(x , y)) 
---
+++ BasicLevel/Swap/Approach4.py
@@ -0,0 +1,6 @@
+x = int(input('Enter x value : '))
+y = int(input('Enter y value : '))
+x = x ^ y 
+y = x ^ y 
+x = x ^ y
+print(' x = {0}  and y = {1} '.format(x , y)) 
