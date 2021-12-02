8a42c9b14b1cb5232788c9cd1f378cb524186f3e
JRShreevalli <70849567+JRShreevalli@users.noreply.github.com>
2021-12-02 12:18:23 +0530

    Create SimpleList.py

---
+++ BasicFunction/List/SimpleList.py
@@ -0,0 +1,11 @@
+list = []
+n = int(input())
+
+for _ in range( 1 , n+1 ):
+    element = int(input())
+    list.append(element)  
+
+print(list)
+
+
+# append will add elements to the list ..............
